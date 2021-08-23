d2b895c4a2e3920228885c5f53fd8d7aac5ac6db
Gregory Marklew <greg_marklew@hotmail..com>
2021-08-23 14:34:14 +0100

    fixed # error in html code

--- icon.js
+++ icon.js
@@ -1,32 +1,32 @@
 class Icon {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-		this.width = 180;
-		this.height = 180;
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = 180;
+        this.height = 180;
 
-		const img = new Image();
-		img.src = "";
-		this.image = img;
-	}
+        const img = new Image();
+        img.src = "images/PngItem_1084794.png"
+        this.image = img;
+    }
 
-	draw() {
-		context.drawImage(this.image, this.x, this.y, this.width, this.height);
-	}
+draw(){
+    context.drawImage(this.image, this.x, this.y, this.width, this.height);
+ }
 
-	top() {
-		return this.y;
-	}
+ top() {
+    return this.y;
+  }
 
-	bottom() {
-		return this.y + this.height;
-	}
+  bottom() {
+    return this.y + this.height;
+  }
 
-	left() {
-		return this.x;
-	}
+  left() {
+    return this.x;
+  }
 
-	right() {
-		return this.x + this.width;
-	}
+  right() {
+     return this.x + this.width;
+  }
 }
--- index.js
+++ index.js
@@ -1,43 +1,42 @@
 const canvas = document.getElementById("myCanvas");
-const context = canvas.getContent("2d");
+const context = canvas.getContext("2d");
 
 const icon = new Icon(50, 50);
 let score = 0;
 
 function updateCanvas() {
-	icon.draw();
-	requestAnimationFrame(updateCanvas);
+  icon.draw();
+  requestAnimationFrame(updateCanvas);
 }
 
 updateCanvas();
 
 canvas.addEventListener("click", (e) => {
-	const coord = {
-		x: e.clientX,
-		y: e.clientY,
-	};
-
-	if (checkClickIcon(coord)) {
-		console.log("x", e.clientX);
-		console.log("y", e.clientY);
-		console.log("clicking icon");
-		score++;
-		scoreElement.innerHTML = score;
-	}
+  const coord = {
+    x: e.clientX,
+    y: e.clientY,
+  };
+  if (checkClickIcon(coord)) {
+    console.log("x", e.clientX);
+    console.log("y", e.clientY);
+    console.log("clikinng icon");
+    score++;
+    scoreElement.innerHTML = score;
+  }
 });
 
 function checkClickIcon(mouseCoordinates) {
-	console.log(icon);
-	return !(
-		mouseCoordinates.x > icon.right() ||
-		mouseCoordinates.x < icon.left() ||
-		mouseCoordinates.y > icon.bottom() ||
-		mouseCoordinates.y < icon.top()
-	);
+  console.log(icon);
+  return !(
+    mouseCoordinates.x > icon.right() ||
+    mouseCoordinates.x < icon.left() ||
+    mouseCoordinates.y > icon.bottom() ||
+    mouseCoordinates.y < icon.top()
+  );
 }
 
 const scoreElement = document.getElementById("score");
-icon.onclick = function () {
+    icon.onclick = function() {
 	score++;
-	scoreElement.innerHTML = score;
-};
+    scoreElement.innerHTML = score;
+}
