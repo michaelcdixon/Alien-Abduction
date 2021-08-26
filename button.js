ad9048afe1475a112beea7c635070c6997bdf521
michaelcdixon <michaelmail7@gmail.com>
2021-08-26 17:05:28 +0100

    Fixed typos

--- button.js
+++ button.js
@@ -144,7 +144,7 @@ function buyHarvest() {
 		harvests = harvests = 1;
 		harvestCost = Math.round(harvestCost * 1.15);
 		document.getElementById("score").innerHTML = score;
-		document.getElementById("havestCost").innerHTML = harvestCost;
+		document.getElementById("harvestCost").innerHTML = harvestCost;
 		document.getElementById("harvests").innerHTML = harvests;
 	}
 }
